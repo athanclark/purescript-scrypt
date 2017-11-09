3cf9a9a1f42a076cfb40709994f39f3a03b12a13
Athan Clark <athan.clark@gmail.com>
2017-11-09 14:24:33 -0700

    better example

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -4,23 +4,22 @@ import Crypto.Scrypt (SCRYPT, scrypt)
 import Data.TextEncoder (encodeUtf8)
 
 import Prelude
+import Control.Monad.Aff (runAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log, error)
 import Unsafe.Coerce (unsafeCoerce)
 
 
-main :: forall e. Eff (console :: CONSOLE, scrypt :: SCRYPT | e) Unit
+main :: _
 main = do
   log "Hello sailor!"
 
-  scrypt
+  void $ runAff (\e -> log $ unsafeCoerce e) (scrypt
     { password: encodeUtf8 "test"
     , salt: encodeUtf8 "salt"
     , n: 1024
     , r: 8
     , p: 1
     , dkLen: 32
-    , onError: error
     , onProgress: \p -> log $ "progress: " <> show p
-    , onComplete: \key -> log $ unsafeCoerce key
-    }
+    })
