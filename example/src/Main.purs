0c39585a9aa6ebcd5bc6be082aa2a3667749072e
Athan Clark <athan.clark@gmail.com>
2017-11-09 14:14:21 -0700

    fixing bower

--- example/src/Main.purs
+++ example/src/Main.purs
@@ -1,9 +1,24 @@
 module Main where
 
+import Crypto.Scrypt (SCRYPT, scrypt)
+import Data.TextEncoder (encodeUtf8)
+
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Console (CONSOLE, log, error)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
+main :: forall e. Eff (console :: CONSOLE, scrypt :: SCRYPT | e) Unit
 main = do
   log "Hello sailor!"
+
+  scrypt
+    { password: encodeUtf8 "test"
+    , salt: encodeUtf8 "salt"
+    , n: 1024
+    , r: 8
+    , p: 1
+    , dkLen: 32
+    , onError: error
+    , onProgress: \p -> log $ "progress: " <> show p
+    , onComplete: \key -> log $ show key
+    }
